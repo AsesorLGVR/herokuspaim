5a4cdc2215b16beed0a0d1b2a25f887cdccdff10
SpanishImmersive <68026156+AsesorLGVR@users.noreply.github.com>
2021-02-28 02:19:45 +0100

    Update app.ts

--- hello-world/src/app.ts
+++ hello-world/src/app.ts
@@ -3,16 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import {
-    Actor,
-    AnimationKeyframe,
-    AnimationWrapMode,
-    ButtonBehavior,
-    Context,
-    Quaternion,
-    TextAnchorLocation,
-    Vector3
-} from '@microsoft/mixed-reality-extension-sdk';
+import * as MRE from '@microsoft/mixed-reality-extension-sdk';
 
 /**
  * The main class of this app. All the logic goes here.
